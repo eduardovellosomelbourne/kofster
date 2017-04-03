e79ea82a8d3df1e01d156008e5e5f19e7820ce00
Eduardo Velloso <Eduardo Velloso>
2017-04-03 11:34:44 +1000

    Serve static files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
 
 // Create route for the root
 app.get('/',function(req,res){
-   res.send('Hello World');
+   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.listen(3000,function(req,res){
